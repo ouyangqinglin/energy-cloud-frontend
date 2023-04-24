259de130bf1501ad9f1173ee5f014cac2f92ba6d
杨剑飞 <1294485765@qq.com>
2023-04-24 18:57:30 +0800

    ✨ feat: 修改加载页

---
+++ src/layouts/loading/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2023-04-24 17:41:27
+ * @LastEditTime: 2023-04-24 17:41:30
+ * @LastEditors: YangJianFei
+ * @FilePath: \energy-cloud-frontend\src\pages\loading\index.tsx
+ */
+
+import React from 'react';
+
+const YtStation: React.FC = (props) => {
+  return <div>加载中</div>;
+};
+
+export default YtStation;
