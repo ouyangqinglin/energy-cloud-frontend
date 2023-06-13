3b30f81098cb859c0125251356aca6483fdda762
zhengchuguo <zhengchuguo@yongtai.com>
2023-06-13 09:19:01 +0800

    fix: solved wrong ws protocol

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -1,6 +1,13 @@
 const isDev = process.env.NODE_ENV === 'development';
 const COMMON_PATH = '/prod-api';
+const getWsProtocol = () => {
+  const { protocol } = location;
+  if (protocol.match(/https/gi)) {
+    return 'wss://';
+  }
+  return 'ws://';
+};
 export const getWebSocketHost = () => {
-  const host = isDev ? 'ws://192.168.3.18' : location.origin;
-  return host + COMMON_PATH;
+  const host = isDev ? '192.168.3.18' : location.host;
+  return getWsProtocol() + host + COMMON_PATH;
 };
