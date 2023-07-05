4d1afaa38d4cb0fa8f8df562ae88bb00ffe611d6
yangjianfei <1294485765@qq.com>
2023-07-05 11:53:39 +0800

    feat: 告警样式修改

--- src/components/Alarm/AlarmTable.tsx
+++ src/components/Alarm/AlarmTable.tsx
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: YangJianFei
  * @Date: 2023-05-25 10:21:56
- * @LastEditTime: 2023-07-05 11:45:10
+ * @LastEditTime: 2023-07-05 11:52:44
  * @LastEditors: YangJianFei
  * @FilePath: \energy-cloud-frontend\src\components\Alarm\AlarmTable.tsx
  */
@@ -245,7 +245,7 @@ const Alarm: React.FC<AlarmProps> = (props) => {
       width: 100,
       fixed: 'right',
       render: (_, record) => {
-        return <ClearOutlined onClick={() => onCleanClick(record)} />;
+        return <ClearOutlined className={styles.cleanUp} onClick={() => onCleanClick(record)} />;
       },
     },
   ];
