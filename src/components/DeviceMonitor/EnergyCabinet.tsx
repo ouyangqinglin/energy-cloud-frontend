384dc6ce1575f0ae8b93b4a4301fa0a41b56818c
yangjianfei <1294485765@qq.com>
2023-07-19 19:59:11 +0800

    feat: 设备详情

---
+++ src/components/DeviceMonitor/EnergyCabinet.tsx
@@ -0,0 +1,96 @@
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2023-07-19 19:58:42
+ * @LastEditTime: 2023-07-19 19:58:48
+ * @LastEditors: YangJianFei
+ * @FilePath: \energy-cloud-frontend\src\components\DeviceMonitor\EnergyCabinet.tsx
+ */
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2023-06-27 14:25:13
+ * @LastEditTime: 2023-06-27 14:25:18
+ * @LastEditors: YangJianFei
+ * @FilePath: \energy-cloud-frontend\src\components\DeviceDetail\EnergyCabinet.tsx
+ */
+
+import React, { useMemo, useState, useCallback } from 'react';
+import { Tabs, Empty } from 'antd';
+import type { TabsProps } from 'antd';
+import Alarm from '@/components/Alarm';
+import LogTable from '@/components/LogTable';
+import { getLogs } from '@/services/equipment';
+import styles from './index.less';
+import RealTime from '@/components/Meter/RealTime';
+import { DeviceDetailType } from './config';
+import EquipInfo from '@/components/EquipInfo';
+import type { DeviceType } from '@/components/EquipInfo/type';
+import Community from '@/components/ScreenDialog/Community';
+import EnergyCabinetImg from '@/assets/image/product/energy-cabinet.png';
+import HistoryData from './HistoryData';
+
+const EnergyCabinet: React.FC<DeviceDetailType> = (props) => {
+  const { id } = props;
+
+  const [loading, setLoading] = useState(false);
+  const [deviceData, setDeviceData] = useState<DeviceType>();
+
+  const onDataChange = useCallback((data) => {
+    setDeviceData(data);
+  }, []);
+
+  const items = useMemo<TabsProps['items']>(() => {
+    return [
+      {
+        label: '实时数据',
+        key: '1',
+        children: (
+          <>
+            <EquipInfo
+              id={id}
+              className="mb20"
+              equipmentImg={EnergyCabinetImg}
+              setLoading={setLoading}
+              buttons={
+                <Community id={id} siteId={deviceData?.siteId} type={deviceData?.paramConfigType} />
+              }
+              onChange={onDataChange}
+            />
+            <RealTime id={id} loading={loading} />
+          </>
+        ),
+      },
+      {
+        label: '历史数据',
+        key: '2',
+        children: <HistoryData />,
+      },
+      {
+        label: '告警',
+        key: '3',
+        children: (
+          <Alarm isStationChild={true} params={{ siteId: deviceData?.siteId, deviceId: id }} />
+        ),
+      },
+      {
+        label: '日志',
+        key: '4',
+        children: <LogTable params={{ id }} request={getLogs} />,
+      },
+      {
+        label: '配置',
+        key: '5',
+        children: <Empty />,
+      },
+    ];
+  }, [id, deviceData, loading]);
+
+  return (
+    <>
+      <Tabs className={styles.tabs} items={items} />
+    </>
+  );
+};
+
+export default EnergyCabinet;
