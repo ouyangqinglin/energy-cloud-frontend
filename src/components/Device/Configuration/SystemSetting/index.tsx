d7f72da212709ea599efa164b1cd2b85d05e21f0
CodeHare <sumu_qr@163.com>
2023-11-03 13:56:27 +0800

    feat: ems参数配置按钮完成

--- src/components/Device/Configuration/SystemSetting/index.tsx
+++ src/components/Device/Configuration/SystemSetting/index.tsx
@@ -6,28 +6,49 @@
  * @LastEditors: YangJianFei
  * @FilePath: \energy-cloud-frontend\src\components\DeviceRealTime\Ems\Run\index.tsx
  */
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import Detail from '@/components/Detail';
 import type { GroupItem } from '@/components/Detail';
 import { emsSystemEnabletems, reportItems } from './config';
 import RemoteUpgrade from '../RemoteUpgrade';
+import Button from 'antd/lib/button';
+import _ from 'lodash';
 
 export type StackProps = {
   deviceId: string;
   productId: string;
   realTimeData?: Record<string, any>;
 };
+
 const SystemSetting: React.FC<StackProps> = (props) => {
   const { realTimeData, deviceId } = props;
+
+  const serSystemEnable = useCallback((_) => {
+    console.log("系统使能设置");
+  
+    // run(record.id);
+  }, []);
+
+  const serCommunicationParams = useCallback((_) => {
+    console.log("通信参数设置");
+    
+    // run(record.id);
+  }, []);
+
+
   const detailGroup = useMemo<GroupItem[]>(() => {
     return [
       {
-        label: <Detail.Label title="系统使能设置">112233</Detail.Label>,
+        label: <Detail.Label title="系统使能设置">
+                <Button type="primary" onClick={() => serSystemEnable(_)}>配置参数</Button>
+               </Detail.Label>,
         items: emsSystemEnabletems,
       },
       {
-        label: <Detail.Label title="通信参数设置" />,
+        label: <Detail.Label title="通信参数设置">
+                  <Button type="primary" onClick={() => serCommunicationParams(_)}>配置参数</Button>
+               </Detail.Label>,
         items: reportItems,
       },
     ];
@@ -43,9 +64,10 @@ const SystemSetting: React.FC<StackProps> = (props) => {
             colon: false,
             labelStyle: { width: 140 },
             valueStyle: { width: '40%' },
+            // column: 2,
           }}
         />
-        <RemoteUpgrade deviceId={deviceId} />
+        {/* <RemoteUpgrade deviceId={deviceId} /> */}
       </div>
     </>
   );
