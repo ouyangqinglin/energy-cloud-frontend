f0baccf535a5e422887fdc9189584759de7008c3
aoshilin <1602893986@qq.com>
2023-11-03 13:59:55 +0800

    feat: 自研ems配置页面开发

--- src/components/Device/Configuration/index.tsx
+++ src/components/Device/Configuration/index.tsx
@@ -1,72 +1,50 @@
 /*
  * @Description:
  * @Author: YangJianFei
- * @Date: 2023-06-27 16:31:19
- * @LastEditTime: 2023-07-04 10:04:53
+ * @Date: 2023-08-31 16:22:51
+ * @LastEditTime: 2023-09-25 14:16:52
  * @LastEditors: YangJianFei
- * @FilePath: \energy-cloud-frontend\src\components\alarm\index.tsx
+ * @FilePath: \energy-cloud-frontend\src\components\Device\Configuration\index.tsx
  */
-import React, { useMemo } from 'react';
-import type { TabsProps } from 'antd';
-import { Tabs } from 'antd';
-import ConfigurationTab from './ConfigurationTab';
-import SystemSetting from './SystemSetting';
-import ConverterSetting from './ConverterSetting';
-import BatterySetting from './BatterySetting';
-import styles from './index.less';
-import { useSubscribe } from '@/hooks';
-import { OnlineStatusEnum } from '@/utils/dictionary';
-import type { DeviceDataType } from '@/services/equipment';
+import React from 'react';
+import Community from './Community';
+import DeviceConfig from './DeviceConfig';
+import { isEmpty } from '@/utils';
+import RemoteSetting from './RemoteSetting';
+import { DeviceTypeEnum } from '@/utils/dictionary';
+import RemoteUpgrade from './RemoteUpgrade';
+import SelfEmsIndex from './SelfEmsIndex';
+
 export type ConfigProps = {
-  deviceId: string;
+  deviceData: any;
   productId: string;
-  deviceData: DeviceDataType;
+  deviceId: string;
 };
-
-const Index: React.FC<ConfigProps> = (props) => {
-  const { deviceId, productId, deviceData } = props;
-  const openSubscribe = useMemo(
-    () => !!deviceData && deviceData?.status !== OnlineStatusEnum.Offline,
-    [deviceData],
+const remoteSettingType = [DeviceTypeEnum.Ems, DeviceTypeEnum.BWattAir];
+const ConfigurationTab: React.FC<ConfigProps> = (props) => {
+  const { deviceData, productId, deviceId } = props;
+  return (
+    <>
+      {productId == '59' ? (
+        <SelfEmsIndex deviceData={deviceData} productId={productId} deviceId={deviceId} />
+      ) : (
+        <div className="px24">
+          {deviceData?.paramConfigType ? <Community deviceData={deviceData} /> : <></>}
+          {!isEmpty(deviceData?.productConfigType) ? (
+            <DeviceConfig deviceData={deviceData} />
+          ) : (
+            <></>
+          )}
+          {remoteSettingType.includes(deviceData?.productId) ? (
+            <RemoteSetting deviceData={deviceData} />
+          ) : (
+            <></>
+          )}
+          <RemoteUpgrade deviceId={deviceData?.deviceId} />
+        </div>
+      )}
+    </>
   );
-  let realTimeData = useSubscribe(deviceId, openSubscribe);
-  // mock
-  realTimeData = {
-    SysEnableSelfStartFunction: 0,
-    MunicipalChargingFunctionEnabled: 1,
-    EnableOffGridOperationFunction: 0,
-    EnableGridConnectionFunction: 1,
-  };
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: `系统设置`,
-      children: (
-        <SystemSetting deviceId={deviceId} productId={productId} realTimeData={realTimeData} />
-      ),
-    },
-    {
-      key: '2',
-      label: `变流器设置`,
-      children: (
-        <ConverterSetting deviceId={deviceId} productId={productId} realTimeData={realTimeData} />
-      ),
-    },
-    {
-      key: '3',
-      label: `电池设置`,
-      children: (
-        <BatterySetting deviceId={deviceId} productId={productId} realTimeData={realTimeData} />
-      ),
-    },
-    {
-      key: '4',
-      label: `能量管理`,
-      children: <ConfigurationTab deviceData={deviceData} />,
-    },
-  ];
-
-  return <Tabs className={styles.tabs} tabBarGutter={34} items={items} />;
 };
 
-export default Index;
+export default ConfigurationTab;
