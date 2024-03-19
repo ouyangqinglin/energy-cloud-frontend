688d1afb7e7e231a3593dad61624df4f53452140
zhupeng <616033286@qq.com>
2024-03-19 19:47:15 +0800

    测试

--- src/pages/screen/customScreen/components/header.tsx
+++ src/pages/screen/customScreen/components/header.tsx
@@ -8,7 +8,7 @@ const Header: FC = () => {
   return (
     <>
       <div>
-        <div className={styles.titleFont}>杰成光储充低碳园区示范站1</div>
+        <div className={styles.titleFont}>杰成光储充低碳园区示范站</div>
         <div className={styles.enFont}>
           Jiecheng Optical Storage and Charging Low Carbon Park Demonstration Station
         </div>
