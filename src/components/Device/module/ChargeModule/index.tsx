0b82ac3e2347232ccb8e42a158d09d2d75df2c2e
yangjianfei <1294485765@qq.com>
2024-03-23 10:59:15 +0800

    feat: 设备详情添加充电桩自定义组件

---
+++ src/components/Device/module/ChargeModule/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2024-03-23 10:58:19
+ * @LastEditTime: 2024-03-23 10:58:48
+ * @LastEditors: YangJianFei
+ * @FilePath: \energy-cloud-frontend\src\components\Device\module\ChargeModule\index.tsx
+ */
+
+import React from 'react';
+
+const ChargeModule: React.FC = () => {
+  return <></>;
+};
+
+export default ChargeModule;
---
+++ src/components/Device/module/PowerSwitch/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2024-03-23 10:57:35
+ * @LastEditTime: 2024-03-23 10:57:35
+ * @LastEditors: YangJianFei
+ * @FilePath: \energy-cloud-frontend\src\components\Device\module\PowerSwitch\index.tsx
+ */
+
+import React from 'react';
+
+const PowerSwitch: React.FC = () => {
+  return <></>;
+};
+
+export default PowerSwitch;
