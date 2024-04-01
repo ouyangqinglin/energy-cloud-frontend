23cd994b605ca6d08b732b7dfd4ad5d59116837b
yangjianfei <1294485765@qq.com>
2024-04-01 18:56:33 +0800

    feat: 修改苹果链接

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: YangJianFei
  * @Date: 2023-07-17 15:45:18
- * @LastEditTime: 2024-04-01 17:59:16
+ * @LastEditTime: 2024-04-01 18:55:39
  * @LastEditors: YangJianFei
  * @FilePath: \energy-cloud-frontend\src\utils\config.ts
  */
@@ -17,6 +17,7 @@ export const adminAuthority = '*:*:*';
 export const defaultSystemInfo = {
   title: '新能源能量管理云平台',
   icon: YTIcon,
-  appStore: '//itunes.apple.com/app/6473716201',
+  appStore:
+    'https://apps.apple.com/cn/app/%E6%B0%B8%E6%B3%B0%E6%96%B0%E8%83%BD%E6%BA%90/id6473716201',
   logo: isZhCN ? YTLogo : YTLogoUS,
 };
