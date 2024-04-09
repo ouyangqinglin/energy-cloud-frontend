b8bc7b642ca6a896649afb672565e99e10568715
yangjianfei <1294485765@qq.com>
2024-04-09 13:55:29 +0800

    feat: 修改数据拦截器

--- src/components/Device/components/interceptor.ts
+++ src/components/Device/components/interceptor.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: YangJianFei
  * @Date: 2024-03-23 14:19:52
- * @LastEditTime: 2024-04-09 11:49:27
+ * @LastEditTime: 2024-04-09 13:55:04
  * @LastEditors: YangJianFei
  * @FilePath: \energy-cloud-frontend\src\components\Device\components\interceptor.ts
  */
@@ -13,7 +13,7 @@ export const flat = (key: string, data: string) => {
   const result: Record<string, any> = {};
   parseToArray(data)?.forEach?.((item, index) => {
     Object.keys(item)?.forEach?.((itemKey) => {
-      result[`${key}[${index}].${itemKey}`] = item[itemKey];
+      result[`${key}[${index}]['${itemKey}']`] = item[itemKey];
     });
   });
   return result;
