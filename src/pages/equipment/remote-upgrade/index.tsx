cc602e91d530ae0a64e29f508bfa34eabe8cdd5d
pengyilei <1079425363@qq.com>
2024-07-18 17:47:45 +0800

    feat: 升级管理-添加权限控制

--- src/pages/equipment/remote-upgrade/index.tsx
+++ src/pages/equipment/remote-upgrade/index.tsx
@@ -6,31 +6,45 @@ import { useAuthority } from '@/hooks';
 import Package from './package';
 import UpgradeTask from './upgradeTask';
 import Log from './log';
+import { Item } from '@ant-design/charts';
+import { formatMessage } from '@/utils';
 
 const UpgradeManage: React.FC = () => {
-  const { authorityMap } = useAuthority(['system:user:authority', 'system:user:org']);
+  // const { authorityMap } = useAuthority(['system:user:authority', 'system:user:org']);
   const intl = useIntl();
-  const items = useMemo<TabsProps['items']>(() => {
-    return [
-      {
+
+  const { authorityMap } = useAuthority([
+    'system:user:authority',
+    'system:user:org',
+    'upgradManage:package:manage',
+    'upgradManage:package:task',
+    'upgradManage:package:log',
+  ]);
+
+  const items = useMemo(() => {
+    const result: TabsProps['items'] = [];
+    if (authorityMap.get('upgradManage:package:manage')) {
+      result.push({
         key: '1',
-        label: intl.formatMessage({
-          id: 'upgradeManage.upgradePack',
-          defaultMessage: '升级包管理',
-        }),
+        label: formatMessage({ id: 'upgradeManage.upgradePack', defaultMessage: '升级包管理' }),
         children: <Package />,
-      },
-      {
+      });
+    }
+    if (authorityMap.get('upgradManage:package:task')) {
+      result.push({
         key: '2',
-        label: intl.formatMessage({ id: 'upgradeManage.upgradeTask', defaultMessage: '升级任务' }),
+        label: formatMessage({ id: 'upgradeManage.upgradeTask', defaultMessage: '升级任务' }),
         children: <UpgradeTask />,
-      },
-      {
+      });
+    }
+    if (authorityMap.get('upgradManage:package:log')) {
+      result.push({
         key: '3',
-        label: intl.formatMessage({ id: 'upgradeManage.upgradeLog', defaultMessage: '升级记录' }),
+        label: formatMessage({ id: 'upgradeManage.upgradeLog', defaultMessage: '升级记录' }),
         children: <Log />,
-      },
-    ];
+      });
+    }
+    return result;
   }, [authorityMap]);
 
   return (
