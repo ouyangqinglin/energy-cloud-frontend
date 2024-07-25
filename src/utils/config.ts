012c92200f0a66620af5334b44bec29a33ef7e86
yangjianfei <1294485765@qq.com>
2024-07-25 09:30:46 +0800

    feat: 修改苹果app链接

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -17,7 +17,6 @@ export const adminAuthority = '*:*:*';
 export const defaultSystemInfo = {
   title: isZhCN ? '新能源能量管理云平台' : 'YT EMS Cloud',
   icon: YTIcon,
-  appStore:
-    'https://apps.apple.com/cn/app/%E6%B0%B8%E6%B3%B0%E6%96%B0%E8%83%BD%E6%BA%90/id6473716201',
+  appStore: 'https://apps.apple.com/app/%E6%B0%B8%E6%B3%B0%E6%96%B0%E8%83%BD%E6%BA%90/id6473716201',
   logo: isZhCN ? YTLogo : YTLogoUS,
 };
